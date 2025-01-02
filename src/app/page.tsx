2917134d065ff41a6c6c85d12f0a63d9caede5de
rimapaulione <rimapaulione@gmail.com>
2025-01-02 15:44:12 +0200

    add backgound image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="bg-black bg-home-bg bg-cover bg-center ">
       <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
-        <div className="text-white">
-          <h1>
+        <div className="flex flex-col gap-6 p-12 rounded-xl bg-black/70 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl">
+          <h1 className="text-4xl font-bold">
             HOME PAGE <br /> SHOP
           </h1>
           <address>
